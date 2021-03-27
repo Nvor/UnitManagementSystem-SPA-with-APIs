b86577c52227ef182e9194e53a43fc5d3f074465
nvor <nickbaripour@gmail.com>
2021-03-27 17:26:43 -0400

    upgraded angular material, flex to v11, updated routing option

--- UnitMonitoringSystemClient/src/app/modules/instance/instance.module.ts
+++ UnitMonitoringSystemClient/src/app/modules/instance/instance.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout'
-import { MatCardModule, MatDividerModule, MatListModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatSelectModule, MatOptionModule, MatInputModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { InstanceComponent } from './instance.component';
 import { UnitsPanelComponent } from './units-panel/units-panel.component';
 import { UnitDetailsPanelComponent } from './unit-details-panel/unit-details-panel.component';
--- UnitMonitoringSystemClient/src/app/shared/shared.module.ts
+++ UnitMonitoringSystemClient/src/app/shared/shared.module.ts
@@ -3,7 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { SidebarComponent } from 'src/app/shared/components/sidebar/sidebar.component';
 import { FooterComponent } from 'src/app/shared/components/footer/footer.component';
-import { MatDividerModule, MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, MatListModule } from '@angular/material'
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 
