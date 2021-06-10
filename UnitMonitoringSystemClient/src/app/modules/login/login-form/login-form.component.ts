91dc0332d48b02b7ec1b0114e1e5c40cf5692c2d
nvor <nickbaripour@gmail.com>
2021-06-09 22:40:34 -0400

    start to login page

---
+++ UnitMonitoringSystemClient/src/app/modules/login/login-form/login-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit(loginForm: FormGroup) {
+    
+  }
+
+}
